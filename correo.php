4a0f0cb333eb10e1695e14e9a0d686225a7c3741
msarevalo <msarevalo@unbosque.edu.co>
2018-01-26 16:02:58 -0500

    correcion correo

--- correo.php
+++ correo.php
@@ -1,5 +1,5 @@
 <?php
-$to = 'soportecolombia@celmedia.com';
+/*$to = 'msscout11@gmail.com';
 $subject = 'Solicitud Pagina WEB!';
 $message = 'Nombre: Soporte' . "\n";
 $message .= 'Email: msscout11@gmail.com' . "\n";
@@ -7,9 +7,37 @@ $headers = 'From: soportecolombia@celmedia.com' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 ini_set("smtp_port", "25");
 //mail ($to, $subject, $message, $headers);
-//header("Location: thanks.html");
-echo "<script>alert('Mensaje enviado al correo'); window.location.href='index.html'</script>";
-$correo = mail("soportecolombia@celmedia.com", "Prueba", "Esto es un mensaje de prueba", "From: Manuel Arevalo <msarevalo@cuentas.com>");
+//header("Location: thanks.html");*/
+// Multiple recipients
+$to = 'msscout11@gmail.com'; // note the comma
 
+// Subject
+$subject = 'Pruebas';
 
+// Message
+$message = '
+<html>
+<head>
+  <title>Birthday Reminders for August</title>
+</head>
+<body>
+  <a href="http://localhost:63342/Cuentas/index.html">Prueba</a>
+</body>
+</html>
+';
+
+// To send HTML mail, the Content-type header must be set
+$headers[] = 'MIME-Version: 1.0';
+$headers[] = 'Content-type: text/html; charset=iso-8859-1';
+
+// Additional headers
+$headers[] = 'To: Manuel <' . $to . '>';
+$headers[] = 'From: Soporte Cuentas <soporte@cuentas.com>';
+
+// Mail it
+if (mail($to, $subject, $message, implode("\r\n", $headers))){
+    echo "<script>alert('Mensaje de reestablecer contraseña enviado'); window.location.href='README.md'</script>";
+}else{
+    echo "<script>alert('Algo ha pasado')</script>";
+}
 ?>
